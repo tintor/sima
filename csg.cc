a1b53402fc5d6154859e3013e90483b1a6dc0331
Marko Tintor <marko@tintor.net>
2018-07-18 00:59:42 -0700

    add empty csh.h/cc

---
+++ csg.cc
@@ -0,0 +1,17 @@
+#include "csg.h"
+
+static imesh3 combine(const imesh3& a, const imesh3& b) {
+	return imesh3();
+}
+
+imesh3 csg_intersection(const imesh3& a, const imesh3& b) {
+	return imesh3();
+}
+
+imesh3 csg_union(const imesh3& a, const imesh3& b) {
+	return imesh3();
+}
+
+imesh3 csg_difference(const imesh3& a, const imesh3& b) {
+	return imesh3();
+}
