c46f98b1dd149376b361935a8f21bcc9b3edf0e5
Marko Tintor <marko@tintor.net>
2018-07-19 09:34:25 -0700

    extract properties.h/cc from shape.h/cc

---
+++ properties.cc
@@ -0,0 +1,71 @@
+#include "properties.h"
+
+// Volume of a valid polyhedron
+double volume(const imesh3& mesh) {
+	double volume = 0;
+	for (const auto& f : mesh) {
+		long s = 0, z = 0;
+		for (auto [a, b] : f.edges()) {
+			z += b.z;
+			s += (long(a.y) + b.y) * (long(a.x) - b.x);
+		}
+		volume += double(z) * s;
+	}
+	return volume / 6;
+}
+
+// TODO test with volume of cube (randomly rotated)
+
+// Center of mass of a valid polyhedron
+ivec3 center_of_mass(const imesh3& mesh) {
+	dvec3 P = {0, 0, 0};
+	double V = 0;
+	for (const auto& f : mesh) {
+		double v = dot(f.a, cross(f.b, f.c));
+		P += dvec3(f.a + f.b + f.c) * v;
+		V += v;
+	}
+	P /= V * 4;
+	return {std::round(P.x), std::round(P.y), std::round(P.z)};
+}
+
+inline dmat3 full_mat3(double a) {
+    const double m[] = { a, a, a, a, a, a, a, a, a};
+    return glm::make_mat3(m);
+}
+
+// Moment of inertia of a valid polyhedron with Center of Mass = 0 and Density = 1
+dmat3 moment_of_inertia(const imesh3& mesh) {
+	constexpr double a = 1 / 60., b = 1 / 120.;
+    constexpr double canon[] = { a, b, b, b, a, b, b, b, a};
+	const dmat3 canonical = glm::make_mat3(canon);
+    dmat3 C = dmat3(0); // covariance
+	for (const auto& f : mesh) {
+		dmat3 A;
+		for (auto i : range(3))
+			glm::row(A, i) = f[i]; // TODO: or column(i) = ?
+		C += (transpose(A) * canonical * A) * determinant(A);
+	}
+	return full_mat3(C[0][0] + C[1][1] + C[2][2]) - C; // C -> I
+}
+
+bool is_aabb(const imesh3& mesh) {
+	ivec3 vmin = mesh[0].a, vmax = mesh[0].a;
+	for (auto f : mesh)
+
+	// All vertices must be made from extreme coordinates
+	for (auto f : mesh)
+		for (ivec3 v : f)
+			if (v.x != vmin.x && v.x != vmax.x && v.y != vmin.y && v.y != vmax.y && v.z != vmin.z && v.z != vmax.z)
+				return false;
+
+	// Every face must have one coordinate constant
+	for (auto f : mesh) {
+		bool xx = f.a.x == f.b.x && f.b.x == f.c.x;
+		bool yy = f.a.y == f.b.y && f.b.y == f.c.y;
+		bool zz = f.a.z == f.b.z && f.b.z == f.c.z;
+		if (!xx && !yy && !zz)
+			return false;
+	}
+	return true;
+}
---
+++ properties.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "triangle.h"
+
+// Volume of a valid polyhedron
+double volume(const imesh3& mesh);
+
+// Center of mass of a valid polyhedron
+ivec3 center_of_mass(const imesh3& mesh);
+
+// Moment of inertia of a valid polyhedron with Center of Mass = 0 and Density = 1
+dmat3 moment_of_inertia(const imesh3& mesh);
+
+bool is_aabb(const imesh3& mesh);
