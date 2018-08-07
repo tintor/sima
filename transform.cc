5f2387aad159dd6df7f5fcb17be58bb87d0fc8f2
Marko Tintor <marko@tintor.net>
2018-08-06 23:36:44 -0700

    transform

---
+++ transform.cc
@@ -0,0 +1,103 @@
+#include "transform.h"
+#include "format.h"
+#include "util.h"
+
+// 7 instructions
+// input a.w is ignored (as long as matrix is fully initialized 4x4)
+// output w is 0
+float4 transform(float4 a, float4 m[4]) {
+    float4 b = m[3];
+    b = fma(a.xxxx, m[0], b);
+    b = fma(a.yyyy, m[1], b);
+    b = fma(a.zzzz, m[2], b);
+    return b;
+}
+
+// transform 2 vectors at once
+// 7 instructions!
+// 3.5 instructions per vector
+// m must be pre-expanded
+float8 transform(float8 a, float8 m[4]) {
+    float8 b = m[3];
+    b = fma(vshuffle(a, a, 0, 0, 0, 0, 4, 4, 4, 4), m[0], b);
+    b = fma(vshuffle(a, a, 1, 1, 1, 1, 5, 5, 5, 5), m[1], b);
+    b = fma(vshuffle(a, a, 2, 2, 2, 2, 6, 6, 6, 6), m[2], b);
+    return b;
+}
+
+// inner loop is 15 instructions for 8 vectors!
+void transform(array_cptr<vec3_8> in, mat34 m, array_ptr<vec3_8> out) {
+	if (in.size() != out.size())
+		THROW(invalid_argument, "array sizes must be the same");
+	if (!aligned<32>(in.begin()) || !aligned<32>(out.begin()))
+		THROW(invalid_argument, "arrays must be 32-byte aligned");
+
+	float8 max = m.a.xxxxxxxx;
+    float8 may = m.a.yyyyyyyy;
+    float8 maz = m.a.zzzzzzzz;
+    float8 maw = m.a.wwwwwwww;
+
+    float8 mbx = m.b.xxxxxxxx;
+    float8 mby = m.b.yyyyyyyy;
+    float8 mbz = m.b.zzzzzzzz;
+    float8 mbw = m.b.wwwwwwww;
+
+    float8 mcx = m.c.xxxxxxxx;
+    float8 mcy = m.c.yyyyyyyy;
+    float8 mcz = m.c.zzzzzzzz;
+    float8 mcw = m.c.wwwwwwww;
+
+	auto a = in.begin();
+	auto b = out.begin();
+	while (a < in.end()) {
+	    float8 v = maw;
+	    v = fma(a->x, max, v);
+	    v = fma(a->y, may, v);
+	    v = fma(a->z, maz, v);
+	    b->x = v;
+
+	    v = mbw;
+   		v = fma(a->x, mbx, v);
+	    v = fma(a->y, mby, v);
+	    v = fma(a->z, mbz, v);
+	    b->y = v;
+
+	    v = mcw;
+	    v = fma(a->x, mcx, v);
+	    v = fma(a->y, mcy, v);
+	    v = fma(a->z, mcz, v);
+	    b->z = v;
+
+		a += 1;
+		b += 1;
+	}
+}
+
+void translate(array_ptr<ivec3_8> vectors, ivec4 t) {
+	if (!aligned<32>(vectors.begin()))
+		THROW(invalid_argument, "address must be 32-byte aligned");
+
+	ivec8 tx = t.xxxxxxxx;
+	ivec8 ty = t.yyyyyyyy;
+	ivec8 tz = t.zzzzzzzz;
+
+	for(auto it = vectors.begin(); it < vectors.end(); it++) {
+		it->x += tx;
+		it->y += ty;
+		it->z += tz;
+	}
+}
+
+void translate(array_ptr<ivec4> vectors, ivec4 offset) {
+	ivec8* it = reinterpret_cast<ivec8*>(vectors.begin());
+	ivec8* end = it + vectors.size() / 2;
+	if (!aligned<32>(vectors.begin()))
+		THROW(invalid_argument, "address must be 32-byte aligned");
+
+	ivec8 offset2 = offset.xyzwxyzw;
+	while (it < end)
+		*it++ += offset2;
+
+	if (vectors.size() % 2 == 1)
+		vectors.back() += offset;
+}
--- transform.h
+++ transform.h
@@ -1,60 +1,58 @@
 #pragma once
+#include "triangle.h"
+#include "array_ptr.h"
 
-#include "glm.h"
-#include <immintrin.h>
-
-typedef float float3 __attribute__((ext_vector_type(3)));
-typedef float float4 __attribute__((ext_vector_type(4)));
-typedef int int4 __attribute__((ext_vector_type(4)));
-
-// __builtin_convertvector
-
-// converts float4 to int4
-// __m128i _mm_cvtps_epi32 (__m128 a)
-// convert int4 to float4
-// __m128 _mm_cvtepi32_ps (__m128i a)
-
-// transform vector by matrix with 5 instructions!
-// +2 instructions if input/output is int4
-inline float4 transform(float4 v, float4 m[3]) {
+inline float4 transform(float4 v, mat34 matrix) {
 	// dot products
-	float4 x = _mm_dp_ps(v, m[0], 0xF1);
-	float4 y = _mm_dp_ps(v, m[1], 0xF2);
-	float4 z = _mm_dp_ps(v, m[2], 0xF4);
-	return x + y + z;
+	float4 x = _mm_dp_ps(v, matrix.a, 0b11110001);
+	float4 y = _mm_dp_ps(v, matrix.b, 0b11110010);
+	float4 z = _mm_dp_ps(v, matrix.c, 0b11110100);
+	float4 q = x + y + z;
+	q.w = v.w;
+	return q;
 }
 
-void translate(imesh3& m, float3 t);
-void transform(imesh3& m, float4 t[3]);
+void transform(array_cptr<vec3_8> in, mat34 matrix, array_ptr<vec3_8> out);
+
+void translate(array_ptr<ivec4> vectors, ivec4 t);
+void translate(array_ptr<ivec3_8> vectors, ivec4 t);
+
+/*inline void translate(imesh3& mesh, ivec3 t) {
+	ivec3* begin = &mesh.begin()->a;
+	ivec3* end = &mesh.begin()->a;
+	translate(array_ptr<ivec3>(begin, end), t);
+}*/
+
+//void transform(imesh3& mesh, float4 t[3]);
 
 struct transform3 {
-	/*dmat3 orientation;
-	dvec3 position;
+	mat34 matrix;
+	/*
 
-    transform3(const dvec3& position, dquat orientation)
+    transform3(const dvec4& position, dvec4 orientation)
         : orientation(orientation), position(position) { }
 
     dvec3 to_global(dvec3 v) const { return orientation * v + position; }
     dvec3 to_local(dvec3 v) const { return (v - position) * orientation; }
 
-    segment3 to_local(const segment3& p) const {
-		return segment3(to_local(p.a), to_local(p.b));
+    isegment3 to_local(const isegment3& p) const {
+		return isegment3(to_local(p.a), to_local(p.b));
 	}
 
 	dvec3 to_global_dir(const dvec3& p) const {
 		return orientation * p;
 	}
 
-	segment3 to_global(const segment3& p) const {
-		return segment3(to_global(p.a), to_global(p.b));
+	isegment3 to_global(const isegment3& p) const {
+		return isegment3(to_global(p.a), to_global(p.b));
 	}
 
-	triangle3 to_local(const triangle3& p) const {
-		return triangle3(to_local(p.a), to_local(p.b), to_local(p.c));
+	itriangle3 to_local(const itriangle3& p) const {
+		return itriangle3(to_local(p.a), to_local(p.b), to_local(p.c));
 	}
 
-	triangle3 to_global(const triangle3& p) const {
-		return triangle3(to_global(p.a), to_global(p.b), to_global(p.c));
+	itriangle3 to_global(const itriangle3& p) const {
+		return itriangle3(to_global(p.a), to_global(p.b), to_global(p.c));
 	}*/
 };
 
