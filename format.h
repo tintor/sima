9a541cbc13836a0c3221cf518fc5492c65b07ccc
Marko Tintor <marko@tintor.net>
2018-08-20 23:24:56 -0700

    format.h: add span, %x for ints/pointers, replacing snprintf for floats

--- format.h
+++ format.h
@@ -7,30 +7,37 @@
 // ostream is heavy).
 #include "int.h"
 #include "std.h"
+#include "span.h"
 
 template<typename... Args>
 void format_s(string& s, string_view fmt, Args... args);
 
 template<typename T>
-void format_e(string& s, string_view spec, const vector<T>& m) {
+void format_span(string& s, string_view spec, span<T> m) {
 	s += '[';
 	if (m.size() > 0) {
-		format_s(s, "%s", m[0]);
-		for (size_t i = 1; i < m.size(); i++)
-			format_s(s, ", %s", m[i]);
+		format_s(s, spec, m[0]);
+		for (size_t i = 1; i < m.size(); i++) {
+			s += ", ";
+			format_s(s, spec, m[i]);
+		}
 	}
 	s += ']';
 }
 
+template<typename T>
+void format_e(string& s, string_view spec, span<T> m) {
+	format_span(s, spec, m);
+}
+
+template<typename T, typename A>
+void format_e(string& s, string_view spec, const vector<T, A>& m) {
+	format_span(s, spec, m);
+}
+
 template<typename T, size_t S>
 void format_e(string& s, string_view spec, const array<T, S>& m) {
-	s += '[';
-	if (m.size() > 0) {
-		format_s(s, "%s", m[0]);
-		for (size_t i = 1; i < m.size(); i++)
-			format_s(s, ", %s", m[i]);
-	}
-	s += ']';
+	format_span(s, spec, m);
 }
 
 template<typename Map>
@@ -64,7 +71,7 @@ void format_set(string& s, string_view spec, const Set& m) {
 		for (const auto& e : m) {
 			if (!first)
 				s += ", ";
-			format_s(s, "%s", e);
+			format_s(s, spec, e);
 			first = false;
 		}
 	}
@@ -96,22 +103,42 @@ void format_int(string& s, string_view spec, Integer v) {
 		s += '0';
 		return;
 	}
-	// TODO hexadecimal if spec == '%x'
+
 	array<char, /*sign*/ 1 + sizeof(v) * 5 / 2> buffer;
 	char* p = buffer.end();
-	if (v < 0) {
-		while (v != 0) {
+	const char* digits = "0123456789ABCDEF";
+
+	if (spec == "%x") {
+		if (v < 0) {
+			while (v != 0) {
+				assert(buffer.begin() < p);
+				*--p = digits[-(v % 16)];
+				v /= 16;
+			}
 			assert(buffer.begin() < p);
-			*--p = '0' - (v % 10);
-			v /= 10;
+			*--p = '-';
+		} else {
+			while (v != 0) {
+				assert(buffer.begin() < p);
+				*--p = digits[v % 16];
+				v /= 16;
+			}
 		}
-		assert(buffer.begin() < p);
-		*--p = '-';
 	} else {
-		while (v != 0) {
+		if (v < 0) {
+			while (v != 0) {
+				assert(buffer.begin() < p);
+				*--p = '0' - (v % 10);
+				v /= 10;
+			}
 			assert(buffer.begin() < p);
-			*--p = '0' + (v % 10);
-			v /= 10;
+			*--p = '-';
+		} else {
+			while (v != 0) {
+				assert(buffer.begin() < p);
+				*--p = '0' + (v % 10);
+				v /= 10;
+			}
 		}
 	}
 	s += string_view(p, buffer.end() - p);
@@ -128,10 +155,141 @@ FORMAT_INT(ulong)
 FORMAT_INT(cent)
 FORMAT_INT(ucent)
 
+template<typename T>
+inline void format_e(string& s, string_view spec, const T* v) { format_int(s, "%x", reinterpret_cast<size_t>(v)); }
+
+// TODO precompute
+inline double POW10(int a) { return std::pow(10, a); }
+
+inline void format_fp(string& s, double fvalue, int min, int max, bool plus, bool minus, bool zero, bool space) {
+        int signvalue = 0;
+        char iconvert[311];
+        char fconvert[311];
+        int iplace = 0;
+        int fplace = 0;
+        int padlen = 0; /* amount to pad */
+        int zpadlen = 0;
+        int index;
+        double intpart;
+        double fracpart;
+        double temp;
+
+        if (max < 0)
+                max = 6;
+
+        double ufvalue = std::abs(fvalue);
+
+        if (fvalue < 0) {
+                signvalue = '-';
+        } else {
+                if (plus) {
+                        signvalue = '+';
+                } else {
+                        if (space)
+                                signvalue = ' ';
+                }
+        }
+
+#if 0
+         if (max == 0) ufvalue += 0.5; /* if max = 0 we must round */
+#endif
+
+        /*
+         * Sorry, we only support 16 digits past the decimal because of our
+         * conversion method
+         */
+        if (max > 16)
+                max = 16;
+
+        /* We "cheat" by converting the fractional part to integer by
+         * multiplying by a factor of 10
+         */
+
+        temp = ufvalue;
+		std::modf(temp, &intpart);
+
+        fracpart = std::round((POW10(max)) * (ufvalue - intpart));
+
+        if (fracpart >= POW10(max)) {
+                intpart++;
+                fracpart -= POW10(max);
+        }
+
+        // Convert integer part
+        do {
+                temp = intpart;
+                std::modf(intpart * 0.1, &intpart);
+                temp = temp * 0.1;
+                index = (int) ((temp - intpart + 0.05) * 10.0);
+                iconvert[iplace++] = '0' + index;
+        } while (intpart && (iplace < 311));
+        if (iplace == 311) iplace--;
+        iconvert[iplace] = 0;
+
+        /* Convert fractional part */
+        if (fracpart) {
+                do {
+                        temp = fracpart;
+						std::modf(fracpart * 0.1, &fracpart);
+                        temp = temp * 0.1;
+                        index = (int) ((temp - fracpart + 0.05) * 10.0);
+                        fconvert[fplace++] = '0' + index;
+                } while(fracpart && (fplace < 311));
+                if (fplace == 311) fplace--;
+        }
+        fconvert[fplace] = 0;
+
+        /* -1 for decimal point, another -1 if we are printing a sign */
+        padlen = min - iplace - max - 1 - (signvalue ? 1 : 0);
+        zpadlen = max - fplace;
+        if (zpadlen < 0)
+				zpadlen = 0;
+        if (padlen < 0)
+                padlen = 0;
+        if (minus)
+                padlen = -padlen; /* Left Justifty */
+        if (zero && (padlen > 0)) {
+                if (signvalue) {
+						s += signvalue;
+                        --padlen;
+                        signvalue = 0;
+                }
+                while (padlen > 0) {
+                        s += '0';
+                        --padlen;
+                }
+        }
+        while (padlen > 0) {
+				s += ' ';
+                --padlen;
+        }
+        if (signvalue)
+                s += signvalue;
+        while (iplace > 0)
+                s += iconvert[--iplace];
+
+        // Decimal point.
+        if (max > 0) {
+				s += '.';
+                while (fplace > 0)
+                        s += fconvert[--fplace];
+        }
+        while (zpadlen > 0) {
+				s += '0';
+                --zpadlen;
+        }
+	while (padlen < 0) {
+		s += ' ';
+		++padlen;
+	}
+}
+
 inline void format_e(string& s, string_view spec, float v) {
-	// TODO pass spec to snprintf
 	array<char, 30> buffer;
-	int length = std::snprintf(buffer.data(), buffer.size(), "%f", v);
+	// TODO don't use string
+	// TODO reset spec to f if lf
+	string _spec(spec);
+	int length = std::snprintf(buffer.data(), buffer.size(), spec == "%s" ? "%g" : _spec.c_str(), v);
 	if (length <= 0)
 		throw std::runtime_error("format: snprintf failed");
 	s += string_view(buffer.data(), length);
@@ -139,7 +297,8 @@ inline void format_e(string& s, string_view spec, float v) {
 
 inline void format_e(string& s, string_view spec, double v) {
 	array<char, 30> buffer;
-	int length = std::snprintf(buffer.data(), buffer.size(), "%lf", v);
+	string _spec(spec);
+	int length = std::snprintf(buffer.data(), buffer.size(), spec == "%s" ? "%g" : _spec.c_str(), v);
 	if (length <= 0)
 		throw std::runtime_error("format: snprintf failed");
 	s += string_view(buffer.data(), length);
@@ -147,7 +306,8 @@ inline void format_e(string& s, string_view spec, double v) {
 
 inline void format_e(string& s, string_view spec, long double v) {
 	array<char, 60> buffer;
-	int length = std::snprintf(buffer.data(), buffer.size(), "%Lf", v);
+	string _spec(spec);
+	int length = std::snprintf(buffer.data(), buffer.size(), spec == "%s" ? "%Lg" : _spec.c_str(), v);
 	if (length <= 0)
 		throw std::runtime_error("format: snprintf failed");
 	s += string_view(buffer.data(), length);
@@ -208,7 +368,7 @@ void format_s(string& s, string_view fmt, Args... args) {
 				break;
 			}
 			if (c == 's' || c == 'f' || c == 'd' || c == 'g' || c == 'x' || c == 'X' || c == 'p') {
-				format_a(s, a++, string_view(q, p - q - 1), args...);
+				format_a(s, a++, string_view(q - 1, p - q + 1), args...);
 				break;
 			}
 		}
--- test_format.cc
+++ test_format.cc
@@ -19,14 +19,19 @@ TEST_CASE("format - basic", "[format]") {
 	REQUIRE("1" == format("%s", int32_t(1)));
 	REQUIRE("123456789" == format("%s", int32_t(123456789)));
 
-	REQUIRE("3.000000" == format("%s", 3.0f));
+	REQUIRE("3.00" == format("%.2f", 3.0f));
+	REQUIRE("3.000000" == format("%f", 3.0f));
+	REQUIRE("3" == format("%s", 3.0f));
+	REQUIRE("3.1" == format("%s", 3.1f));
+	REQUIRE("3.14" == format("%s", 3.14f));
+
 	REQUIRE("" == format(""));
 	REQUIRE_THROWS_WITH(format("%s"), "format: not enough arguments");
 	REQUIRE("%" == format("%%"));
 	REQUIRE("e" == format("e"));
 	REQUIRE("e%" == format("e%%"));
 	REQUIRE("e1b" == format("e%sb", 1));
-	REQUIRE("e1b2.300000ctruea" == format("e%sb%sc%sa", 1, 2.3, true));
+	REQUIRE("e1b2.3ctruea" == format("e%sb%sc%sa", 1, 2.3, true));
 }
 
 TEST_CASE("format - vector", "[format]") {
