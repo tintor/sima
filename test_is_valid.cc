d021862371f867d671d579d5f982bc0d36bb3804
Marko Tintor <marko@tintor.net>
2018-08-23 21:09:32 -0700

    add tests for is_valid(polygon2)

---
+++ test_is_valid.cc
@@ -0,0 +1,44 @@
+#include "catch.hpp"
+#include "is_valid.h"
+#include "string_util.h"
+
+bool is_valid2(string_view text) {
+	polygon2 poly;
+	for (string_view t : split(text, ',')) {
+		auto s = split(t, ' ');
+		REQUIRE(s.size() == 2);
+		poly.push_back(double2{parse<double>(s[0]), parse<double>(s[1])});
+	}
+	return is_valid(poly);
+}
+
+TEST_CASE("is_valid(polygon2)", "[is_valid]") {
+	// not enough points
+	REQUIRE(!is_valid2(""));
+	REQUIRE(!is_valid2("0 0"));
+	REQUIRE(!is_valid2("0 0, 0 1"));
+
+	// valid triangle
+	REQUIRE(is_valid2("0 0, 0 1, 1 0"));
+	// valid triangle (opposite orientation)
+	REQUIRE(is_valid2("0 0, 1 0, 0 1"));
+	// valid square
+	REQUIRE(is_valid2("0 0, 1 0, 1 1, 0 1"));
+
+	// repeated point
+	REQUIRE(!is_valid2("0 0, 0 0, 0 1, 1 0"));
+
+	// self-overlaping edge
+	REQUIRE(!is_valid2("0 0, 2 0, 1 0, 0 1"));
+
+	// bow-tie without shared vertex
+	REQUIRE(!is_valid2("0 0, 2 2, 2 0, 0 2"));
+
+	// bow-tie with shared vertex
+	REQUIRE(!is_valid2("0 0, 1 1, 2 2, 2 0, 1 1, 0 2"));
+
+	// vertex touching edge inside
+	REQUIRE(!is_valid2("0 0, 1 2, 2 0, 2 2, 0 2"));
+	// edge touching edge inside
+	REQUIRE(!is_valid2("0 0, 1 2, 1.1 2, 2 0, 2 2, 0 2"));
+}
