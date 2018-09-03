046a1485a5bf2f7fc946c80b3bcd7fd8c9024f18
Marko Tintor <marko@tintor.net>
2018-09-02 18:51:59 -0700

    add holes to xmesh faces

--- mesh.h
+++ mesh.h
@@ -17,52 +17,71 @@
 //
 // used when constructing meshes directly
 
-using face = span<const point3>;
+template<typename T>
+class array_iterator {
+	uint _pos;
+	const T& _array;
+public:
+	array_iterator(uint pos, const T& a) : _pos(pos), _array(a) { }
+	auto operator*() const { return _array[_pos]; }
+	array_iterator& operator++() { _pos++; return *this; }
+	bool operator!=(array_iterator other) const { return _pos != other._pos; }
+};
+
+// face can have arbitrary number of vertices and holes
+// first ring is exterior, other rings are interior and oriented opposite
+struct face {
+	face(const point3* vertices, span<const uint> offsets) : _vertices(vertices), _offsets(offsets) { }
+	span<const point3> operator[](uint idx) const {
+		return span<const point3>(_vertices + _offsets[idx], _vertices + _offsets[idx + 1]);
+	}
+	uint size() const { return _offsets.size() - 1; }
+	uint vertex_size() const { return _offsets.back() - _offsets[0]; }
+
+	span<const uint> offsets() const { return _offsets; }
+	span<const point3> vertices() const {
+		return span<const point3>(_vertices + _offsets[0], _vertices + _offsets.back());
+	}
+
+	auto begin() const { return array_iterator<face>(0, *this); }
+	auto end() const { return array_iterator<face>(size(), *this); }
+private:
+	const point3* _vertices;
+	span<const uint> _offsets;
+};
 
 class xmesh3 {
 public:
-	xmesh3() { _offsets.push_back(0); }
+	xmesh3() {
+		_offsets.push_back(0);
+		_ring_offsets.push_back(0);
+	}
 
 	void add(face f) {
-		_offsets.push_back(_offsets.back() + f.size());
-		_vertices.resize(_vertices.size() + f.size());
-		std::copy(f.begin(), f.end(), _vertices.end() - f.size());
+		_vertices << f.vertices();
+
+		_offsets.reserve(_offsets.size() + f.offsets().size() - 1);
+		uint a = _offsets.back();
+		for (uint offset : f.offsets().pop_front())
+			_offsets.push_back(offset + a);
+
+		_ring_offsets.push_back(_ring_offsets.back() + f.size());
 	}
 
-	uint size() const { return _offsets.size() - 1; }
+	uint size() const { return _ring_offsets.size() - 1; }
 
-	span<const point3> operator[](uint idx) const {
-		return span<const point3>(_vertices.data() + _offsets[idx], _vertices.data() + _offsets[idx + 1]);
+	face operator[](uint idx) const {
+		span<const uint> offsets(_offsets.data() + _ring_offsets[idx], _offsets.data() + _ring_offsets[idx + 1]);
+		return face(_vertices.data(), offsets);
 	}
 
-	// might not be needed for convex hull after all!
-	void erase(uint idx) {
-		uint s = _offsets[idx + 1] - _offsets[idx];
-		for (uint i = _offsets[idx]; i < _vertices.size() - s; i++)
-			_vertices[i] = _vertices[i + s];
-		_vertices.resize(_vertices.size() - s);
-		for (uint i = idx + 1; i < _offsets.size() - 1; i++)
-			_offsets[i] = _offsets[i + 1] - s;
-		_offsets.resize(_offsets.size() - 1);
-	}
-
-	class iterator {
-		uint _pos;
-		const xmesh3& _mesh;
-	public:
-		iterator(uint pos, const xmesh3& mesh) : _pos(pos), _mesh(mesh) { }
-		face operator*() const { return _mesh[_pos]; }
-		iterator& operator++() { _pos++; return *this; }
-		bool operator!=(iterator other) const { return _pos != other._pos; }
-	};
-
-	iterator begin() const { return iterator(0, *this); }
-	iterator end() const { return iterator(size(), *this); }
+	auto begin() const { return array_iterator<xmesh3>(0, *this); }
+	auto end() const { return array_iterator<xmesh3>(size(), *this); }
 
 	span<const point3> vertices() const { return _vertices; }
-	span<const uint> offsets() const { return _offsets; }
 
 private:
 	aligned_vector<point3> _vertices;
 	vector<uint> _offsets;
+	vector<uint> _ring_offsets;
 };
