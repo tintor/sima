0729863a46120f0b25493eb04729c8a3412d2d55
Marko Tintor <marko@tintor.net>
2019-12-27 14:26:41 -0800

    add natural_less() to string_util.h

--- core/string_util.h
+++ core/string_util.h
@@ -52,3 +52,44 @@ inline vector<string_view> split(string_view s, char delim = ' ') {
 		out.push_back(string_view(b, c));
 	return out;
 }
+
+inline bool is_digit(char c) {
+	return '0' <= c && c <= '9';
+}
+
+// TODO make it work for large numbers
+inline bool natural_less(string_view a, string_view b) {
+	auto ai = a.begin(), bi = b.begin();
+	while (ai != a.end() && bi != b.end()) {
+		bool an = false, bn = false;
+		size_t av, bv;
+
+		if (is_digit(*ai)) {
+			an = true;
+			av = 0;
+			while (ai != a.end() && is_digit(*ai)) {
+				av = av * 10 + (*ai - '0');
+				ai++;
+			}
+		} else
+			av = *ai++;
+
+		if (is_digit(*bi)) {
+			bn = true;
+			bv = 0;
+			while (bi != b.end() && is_digit(*bi)) {
+				bv = bv * 10 + (*bi - '0');
+				bi++;
+			}
+		} else
+			bv = *bi++;
+
+		if (an && !bn)
+			av = '0';
+		if (!an && bn)
+			bv = '0';
+		if (av != bv)
+			return av < bv;
+	}
+	return ai == a.end() && bi != b.end();
+}
--- core/test_util.cc
+++ core/test_util.cc
@@ -1,5 +1,4 @@
 #include <core/util.h>
-#include <core/string_util.h>
 #include <catch.hpp>
 
 TEST_CASE("min", "[util]") {
@@ -7,14 +6,4 @@ TEST_CASE("min", "[util]") {
 	REQUIRE(min(5, 7, 9) == 5);
 	REQUIRE(min(9, 7, 5) == 5);
 	REQUIRE(min(3, 0, -1, 6, 10, -4, 3) == -4);
-}
-
-TEST_CASE("split", "[util]") {
-	using namespace std::literals;
-	REQUIRE(split(""sv) == vector<string_view>{});
-	REQUIRE(split("x"sv) == vector{"x"sv});
-	REQUIRE(split(" a ana b[anana  "sv) == vector{"a"sv, "ana"sv, "b[anana"sv});
-
-	REQUIRE(split(" an|a ba|na"sv) == vector{"an|a"sv, "ba|na"sv});
-	REQUIRE(split(" an|a ba|na"sv, '|') == vector{" an"sv, "a ba"sv, "na"sv});
 }
