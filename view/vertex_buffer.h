08961ae533f2647f8a517e07882ccd4b34c5c002
Marko Tintor <marko@tintor.net>
2020-01-20 23:22:33 -0800

    add VertexBuffer_vec2_rgba to view/vertex_buffer.h

--- view/vertex_buffer.h
+++ view/vertex_buffer.h
@@ -26,10 +26,58 @@ public:
 	}
 
 	void draw(uint type, cspan<double2> vertices) {
+		write(vertices);
+		glDrawArrays(type, 0, vertices.size());
+	}
+
+	void bind() {
+		glBindVertexArray(m_vao);
+	}
+
+private:
+	uint m_vbo, m_vao;
+};
+
+class VertexBuffer_vec2_rgba {
+public:
+	struct Vertex {
+		vec2 pos;
+		uint64_t color;
+	};
+
+	vector<Vertex> data;
+
+	VertexBuffer_vec2_rgba(uint count) {
+		data.reserve(count);
+
+		glGenVertexArrays(1, &m_vao);
+		glGenBuffers(1, &m_vbo);
+
 		glBindVertexArray(m_vao);
 		glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
-		glBufferSubData(GL_ARRAY_BUFFER, 0, vertices.size() * sizeof(double2), vertices.data());
-		glDrawArrays(type, 0, vertices.size());
+		glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * count, nullptr, GL_DYNAMIC_DRAW);
+
+		Vertex* v = nullptr;
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 2, GL_FLOAT, false, sizeof(Vertex), &v->pos);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 4, GL_UNSIGNED_BYTE, true, sizeof(Vertex), &v->color);
+	}
+
+	void add(vec2 pos, uint64_t color) {
+		data.push_back(Vertex{pos, color});
+	}
+
+	void add(double2 pos, uint64_t color) {
+		data.push_back(Vertex{vec2(pos.x, pos.y), color});
+	}
+
+	void draw(uint type) {
+		glBindVertexArray(m_vao);
+		glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, data.size() * sizeof(Vertex), data.data());
+		glDrawArrays(type, 0, data.size());
+		data.clear();
 	}
 
 	void bind() {
@@ -60,9 +108,7 @@ public:
 	}
 
 	void draw(uint type, cspan<vec2> vertices) {
-		glBindVertexArray(m_vao);
-		glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
-		glBufferSubData(GL_ARRAY_BUFFER, 0, vertices.size() * sizeof(vec2), vertices.data());
+		write(vertices);
 		glDrawArrays(type, 0, vertices.size());
 	}
 
