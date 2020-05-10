2da2fe7d5961d0f50123f64a9ef74c836a1e5b3c
Marko Tintor <marko@tintor.net>
2020-05-10 13:46:22 -0700

    Add Values class for Board

--- santorini/board.h
+++ santorini/board.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <fstream>
 #include <core/hash.h>
 #include <core/std.h>
 #include <santorini/cell.h>
@@ -42,32 +43,29 @@ Board Normalize(const Board& board) {
     return out;
 }
 
-// Symmetrical boards are equal!
-bool Equal(const Board& a, const Board& b) {
-    if (a.setup != b.setup || a.player != b.player || a.moved != b.moved || a.built != b.built) return false;
-    for (int t = 0; t < 8; t++) {
-        if (All(kAll, [&](Coord e) { return a.cell[e.v] == b.cell[Transform(e, t).v]; })) return true;
-    }
-    return false;
+bool operator==(const Board& a, const Board& b) {
+    return a.setup == b.setup && a.player == b.player && a.moved == b.moved && a.built == b.built && a.cell == b.cell;
 }
 
-size_t Hash(const array<Cell, 25>& cell, int transform) {
+size_t Hash(const Cells& cell) {
     hash h;
-    for (Coord e : kAll) h << Hash(cell[Transform(e, transform).v]);
+    for (Coord e : kAll) h << Hash(cell[e.v]);
     return h.seed;
 }
 
-// Symmetrical boards will have the same hash!
 size_t Hash(const Board& a) {
     hash h;
     h << a.setup << int(a.player) << a.moved.has_value();
     if (a.moved.has_value()) h << a.moved->v;
-    h << a.built;
-
-    size_t m = 0;
-    for (int t = 0; t < 8; t++) m ^= Hash(a.cell, t);
-    h << m;
+    h << a.built << Hash(a.cell);
     return h.seed;
+}
+
+namespace std {
+    template<>
+    struct hash<Board> {
+        size_t operator()(const Board& b) const { return Hash(b); }
+    };
 }
 
 template <typename Fn>
@@ -77,16 +75,75 @@ int Count(const Board& board, const Fn& fn) {
 
 // Network input description:
 // Board:
-// 0/1 - setup
-// 0/1 - moved
-// 0/1 - built
-// 0/1 - player
+// 1 - setup
+// 25 - which figure moved (if any)
+// 1 - built
+// 1 - player
 // 25x - cell
-// Cell:
-// 0/3 - level
-// 0/1 - dome
-// 0/1 - player1
-// 0/1 - player2
 
-// Output:
-// 0..1 - value function
+// Cell:
+// 4 - level
+// 1 - dome
+// 1 - player1
+// 1 - player2
+
+using std::ostream;
+
+inline char Bit(bool a) { return a ? '1' : '0'; }
+
+ostream& operator<<(ostream& os, Cell cell) {
+    os << Bit(cell.level == 0);
+    os << Bit(cell.level == 1);
+    os << Bit(cell.level == 2);
+    os << Bit(cell.level == 3);
+    os << Bit(cell.figure == Figure::Dome);
+    os << Bit(cell.figure == Figure::Player1);
+    os << Bit(cell.figure == Figure::Player2);
+    return os;
+}
+
+ostream& operator<<(ostream& os, const Board& board) {
+    os << Bit(board.setup);
+    for (Coord e : kAll)
+        os << Bit(board.moved && *board.moved == e);
+    os << Bit(board.built);
+    os << Bit(board.player == Figure::Player1);
+    for (Coord e : kAll)
+        os << board(e);
+    return os;
+}
+
+class Values {
+private:
+    struct Wins {
+        uint32_t player1, player2;
+    };
+
+public:
+    size_t Size() const { return m_data.size(); }
+
+    void Add(const Board& board, uint32_t wins1, uint32_t wins2) {
+        auto result = m_data.emplace(Normalize(board), Wins{0, 0});
+        auto& wins = result.first->second;
+        wins.player1 += wins1;
+        wins.player2 += wins2;
+    }
+
+    optional<double> Value(const Board& board) const {
+        auto it = m_data.find(Normalize(board));
+        if (it == m_data.end()) return nullopt;
+        const auto& wins = it->second;
+        return ((board.player == Figure::Player1) ? wins.player1 : wins.player2) / double(wins.player1 + wins.player2);
+    }
+
+    void Export(string_view filename) {
+        std::ofstream os((string(filename)));
+        for (const auto& [board, wins] : m_data) {
+            double w = wins.player1 / double(wins.player1 + wins.player2);
+            os << board << ' ' << std::setprecision(10) << w << '\n';
+        }
+    }
+
+private:
+    unordered_map<Board, Wins> m_data;
+};
