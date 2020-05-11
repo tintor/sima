3e28b9f61db260d26f4718cabcff973a94609bf5
Marko Tintor <marko@tintor.net>
2020-05-10 20:23:31 -0700

    Working on neural.h and tensor.h

--- santorini/neural.h
+++ santorini/neural.h
@@ -2,144 +2,266 @@
 #include <core/std.h>
 #include <santorini/tensor.h>
 
+template<typename T>
+vector<T> Sub(const vector<T>& in, size_t start, size_t end) {
+    vector<T> out;
+    out.reserve(end - start);
+    for (size_t i = start; i < end; i++) out.push_back(in[i]);
+    return out;
+}
+
+template<typename T>
+vector<T> Sub(const vector<T>& in, size_t start) {
+    return Sub(in, start, in.size());
+}
+
 class Layer {
    public:
-    Layer(cspan<uint32_t> shape) : m_output(shape) {}
+    Layer(cspan<uint32_t> shape) : m_y(shape), m_dy(shape) {}
 
-    const Tensor<float>& Output() const { return m_output; }
-    size_t Size() const { return m_output.size(); }
-    float operator[](size_t index) const { return m_output[index]; }
-    float operator()(cspan<uint32_t> index) const { return m_output(index); }
+    const Tensor<float>& y() const { return m_y; }
+    Tensor<float>& dy() { return m_dy; }
+
+    auto shape() const { return m_y.shape(); }
+    size_t size() const { return m_y.size(); }
 
     void Forward() {}
     void Backward() {}
 
    protected:
-    Tensor<float> m_output;
+    Tensor<float> m_y, m_dy;
 };
 
 class InputLayer : public Layer {
    public:
     InputLayer(cspan<uint32_t> shape) : Layer(shape) {}
-    float& operator()(cspan<uint32_t> index) { return m_output(index); }
-    auto& Tensor() { return m_output; }
+    void set(const TensorSpan<const float>& y) { Check(m_y.shape() == y.shape()); m_y = y; }
 };
 
-inline double Relu(double a) { return a > 0 ? a : 0; }
+inline double Relu(double x) { return x > 0 ? x : 0; }
+
+inline double ReluGradient(double x) {
+    if (x > 0) return 1;
+    if (x == 0) return 0.5;
+    return 0;
+}
 
 class ReluLayer : public Layer {
    public:
-    ReluLayer(const Layer* input) : Layer(input->Output().shape()), m_input(input) {}
+    ReluLayer(Layer* input) : Layer(input->shape()), m_input(input) {}
 
     void Forward() {
-        for (size_t i = 0; i < m_output.size(); i++) m_output[i] = Relu(m_input->Output()[i]);
+        const auto& x = m_input->y();
+        for (size_t i = 0; i < m_y.size(); i++) m_y[i] = Relu(x[i]);
     }
 
-    void Backward() {}
+    void Backward() {
+        const auto& x = m_input->y();
+        auto& dx = m_input->dy();
+        for (size_t i = 0; i < x.size(); i++) dx[i] = m_dy[i] * ReluGradient(x[i]);
+    }
 
    private:
-    const Layer* m_input;
+    Layer* m_input;
 };
 
-inline double Sigmoid(double a) { return 1 / (1 + exp(a)); }
+inline double Sigmoid(double x) { return 1 / (1 + exp(-x)); }
 
 class SigmoidLayer : public Layer {
    public:
-    SigmoidLayer(const Layer* input) : Layer(input->Output().shape()), m_input(input) {}
+    SigmoidLayer(Layer* input) : Layer(input->shape()), m_input(input) {}
 
     void Forward() {
-        const auto& input = m_input->Output();
-        for (size_t i = 0; i < input.size(); i++) m_output[i] = Sigmoid(input[i]);
+        const auto& x = m_input->y();
+        for (size_t i = 0; i < x.size(); i++) m_y[i] = Sigmoid(x[i]);
     }
 
-    void Backward() {}
+    void Backward() {
+        const auto& x = m_input->y();
+        auto& dx = m_input->dy();
+        for (size_t i = 0; i < x.size(); i++) dx[i] = m_dy[i] * m_y[i] * (1 - m_y[i]);
+    }
 
    private:
-    const Layer* m_input;
+    Layer* m_input;
 };
+
+inline float Dot(const TensorSpan<const float>& a, const TensorSpan<const float>& b) {
+    Check(a.size() == b.size());
+    float sum = 0;
+    for (size_t i = 0; i < a.size(); i++) sum += a[i] * b[i];
+    return sum;
+}
+
+vector<uint32_t> Concat(uint32_t a, cspan<uint32_t> b) {
+    vector<uint32_t> out(1 + b.size());
+    out[0] = a;
+    Copy(b, span<uint32_t>(out.data() + 1, out.size() - 1));
+    return out;
+}
 
 class FullyConnectedLayer : public Layer {
    public:
-    FullyConnectedLayer(const Layer* input, uint32_t size, float variance, std::mt19937& random)
-        : Layer({size}), m_input(input), m_weights({uint32_t(size * input->Output().size())}), m_biases({size}) {
+    FullyConnectedLayer(Layer* input, uint32_t size, float variance, std::mt19937& random)
+        : Layer({size}), m_input(input), m_w(Concat(size, input->shape())), m_b({size}, 0.0f) {
         std::normal_distribution<float> dis(0.0f, variance);
-        for (size_t i = 0; i < m_weights.size(); i++) m_weights[i] = dis(random);
-        for (size_t i = 0; i < m_biases.size(); i++) m_biases[i] = 0.0f;
+        for (size_t i = 0; i < m_w.size(); i++) m_w[i] = dis(random);
     }
 
     void Forward() {
-        const auto& input = m_input->Output();
-        const size_t m = input.size();
-        const size_t n = m_biases.size();
+        const auto& x = m_input->y();
+        const size_t m = x.size();
+        const size_t n = m_b.size();
 
         for (size_t i = 0; i < n; i++) {
-            float v = m_biases[i];
+            float s = m_b[i];
             // TODO vectorize
-            for (size_t j = 0; j < m; j++) v += m_weights[i * m + j] * input[j];
-            m_output[i] = v;
+            for (size_t j = 0; j < m; j++) s += m_w[i * m + j] * x[j];
+            m_y[i] = s;
         }
     }
 
-    void Backward() {}
+    void Backward() {
+        const auto& x = m_input->y();
+        auto& dx = m_input->dy();
+        const size_t m = x.size();
+        const size_t n = m_b.size();
+
+        for (size_t j = 0; j < m; j++) {
+            float s = 0;
+            for (size_t i = 0; i < n; i++) s += m_dy[i] * m_w[i * m + j];
+            dx[j] = s;
+        }
+    }
 
    private:
-    const Layer* m_input;
-    Tensor<float> m_weights, m_biases;
+    Layer* m_input;
+    Tensor<float> m_w, m_b;
+};
+
+class MeanSquareErrorLayer : public Layer {
+   public:
+    MeanSquareErrorLayer(Layer* input, const Layer* reference) : Layer({1}), m_input(input), m_reference(reference) {
+        Check(input->shape() == reference->shape());
+    }
+
+    void Forward() {
+        const auto& x = m_input->y();
+        const auto& r = m_reference->y();
+        float error = 0;
+        for (size_t i = 0; i < x.size(); i++) {
+            float e = x[i] - r[i];
+            error += e * e;
+        }
+        m_y[0] = error;
+    }
+
+    void Backward() {
+        const auto& x = m_input->y();
+        const auto& r = m_reference->y();
+        auto& dx = m_input->dy();
+
+        for (size_t i = 0; i < x.size(); i++)
+           dx[i] = 2 * (x[i] - r[i]);
+    }
+
+   private:
+    const Layer* m_reference;
+    Layer* m_input;
 };
 
 struct FeedForwardNetwork {
     InputLayer input;
+    InputLayer reference;
+
+    // hidden
     FullyConnectedLayer fc1;
     ReluLayer relu;
     FullyConnectedLayer fc2;
-    SigmoidLayer sigmoid;
+
+    SigmoidLayer output;
+    MeanSquareErrorLayer loss;
 
     FeedForwardNetwork(uint32_t input_size, uint32_t fc1_size, std::mt19937& random)
         : input({input_size}),
+          reference({1}),
           fc1(&input, fc1_size, 0.01f, random),
           relu(&fc1),
           fc2(&relu, 1, 0.01f, random),
-          sigmoid(&fc2) {}
+          output(&fc2),
+          loss(&output, &reference) {}
 
     void Load(istream& is) {
-        // is >> input >> fc1 >> relu >> fc2 >> sigmoid;
+        // is >> input >> fc1 >> relu >> fc2 >> sigmoid >> loss;
     }
 
     void Save(ostream& os) const {
-        // os << input << fc1 << relu << fc2 << sigmoid;
+        // os << input << fc1 << relu << fc2 << sigmoid << loss;
     }
 
     void Forward() {
-        input.Forward();
         fc1.Forward();
         relu.Forward();
         fc2.Forward();
-        sigmoid.Forward();
+        output.Forward();
     }
 
     void Backward() {
-        sigmoid.Backward();
+        output.Backward();
         fc2.Backward();
         relu.Backward();
         fc1.Backward();
-        input.Backward();
     }
 
-    void TrainBatch1(const Tensor<float>& in, const Tensor<float>& out) {
-        Check(out.shape().size() == 1);
-        Check(out.shape()[0] == 1);
-        input.Tensor() = in;
+    void Train(const TensorSpan<const float>& in, const TensorSpan<const float>& ref) {
+        input.set(in);
+        reference.set(ref);
+
         Forward();
-        float loss = out[0] - sigmoid[0];
+        loss.Forward();
         // TODO how to propagate loss back in network?
+        loss.Backward();
         Backward();
     }
 
-    void TrainBatchN(const Tensor<float>& in, const Tensor<float>& out) {}
+    void TrainBatch(const TensorSpan<const float>& in, const TensorSpan<const float>& ref) {
+        // TODO(Marko)
+    }
+
+    float Loss(const TensorSpan<const float>& in, const TensorSpan<const float>& ref) {
+        input.set(in);
+        reference.set(ref);
+        Forward();
+        loss.Forward();
+        return loss.y()[0];
+    }
 
     float Predict(const Tensor<float>& in) {
-        input.Tensor() = in;
+        input.set(in);
         Forward();
-        return sigmoid[0];
+        return output.y()[0];
     }
 };
+
+void TrainWithSGD(FeedForwardNetwork& model, const Tensor<float>& in, const Tensor<float>& out, std::mt19937& random) {
+    Check(in.shape().size() > 0);
+    Check(out.shape().size() > 0);
+    Check(in.shape().back() == out.shape().back());
+
+    Check(in.shape().pop_back() == model.input.shape());
+    Check(out.shape().pop_back() == model.output.shape());
+
+    vector<uint32_t> samples;
+    samples.resize(in.shape().back());
+    for (uint32_t i = 0; i < samples.size(); i++) samples[i] = i;
+    std::shuffle(samples.begin(), samples.end(), random);
+
+    for (uint32_t index : samples) {
+        model.Train(in.sub(index), out.sub(index));
+        // x
+        // y = f(x, w)
+        // loss = norm(y - ref)
+        // optimize loss == 0  ==>  norm(f(x, w) - ref) == 0  ==>  w -= alpha *  d/dw f(x, w)
+        // w -= alpha * d(x, w)
+    }
+}
--- santorini/tensor.h
+++ santorini/tensor.h
@@ -9,22 +9,32 @@ T Product(const Range& range) {
 }
 
 template <typename CollectionA, typename CollectionB>
-void Copy(const CollectionA& a, CollectionB& b) {
+void Copy(const CollectionA& a, const CollectionB& b) {
     b.resize(a.size());
     for (size_t i = 0; i < a.size(); i++) b[i] = a[i];
 }
 
 template <typename T>
-class Tensor {
+class Tensor;
+
+template <typename T>
+class TensorSpan {
    public:
-    Tensor(cspan<uint32_t> shape) : m_shape(shape.size()), m_data(Product<uint32_t>(shape)) { Copy(shape, m_shape); }
+    TensorSpan(T* data, cspan<uint32_t> shape) : m_data(data) { Copy(shape, m_shape); }
 
-    size_t size() const { return m_data.size(); }
-    const auto& shape() const { return m_shape; }
+    template<typename X>
+    TensorSpan(const TensorSpan<X>& o) : m_data(o.m_data) { Copy(o.m_shape, m_shape); }
+    template<typename X>
+    TensorSpan(const Tensor<X>& o) : m_data(o.data()) { Copy(o.shape(), m_shape); }
 
+    T* data() { return m_data; }
+    T const* data() const { return m_data; }
+
+    size_t size() const { return Product<size_t>(m_shape); }
     T& operator[](size_t index) { return m_data[index]; }
     const T& operator[](size_t index) const { return m_data[index]; }
 
+    const auto& shape() const { return m_shape; }
     T& operator()(cspan<uint32_t> index) { return m_data[offset(index)]; }
     const T& operator()(cspan<uint32_t> index) const { return m_data[offset(index)]; }
 
@@ -32,6 +42,56 @@ class Tensor {
         size_t out = index[0];
         for (size_t i = 1; i < index.size(); i++) out = out * m_shape[i] + index[i];
         return out;
+    }
+
+   private:
+    T* m_data;
+    vector<uint32_t> m_shape;
+};
+
+template <typename T>
+class Tensor {
+   public:
+    Tensor(cspan<uint32_t> shape, T init = T()) : m_data(Product<size_t>(shape), init), m_shape(shape.data(), shape.size()) { }
+    template<typename X>
+    Tensor(const TensorSpan<X>& o) { operator=(o); }
+    Tensor(const Tensor<const T>& o) { operator=(o); }
+
+    size_t size() const { return m_data.size(); }
+    T& operator[](size_t index) { return m_data[index]; }
+    const T& operator[](size_t index) const { return m_data[index]; }
+
+    cspan<uint32_t> shape() const { return m_shape; }
+    T& operator()(cspan<uint32_t> index) { return m_data[offset(index)]; }
+    const T& operator()(cspan<uint32_t> index) const { return m_data[offset(index)]; }
+
+    operator TensorSpan<T>() { return TensorSpan<T>(m_data.data(), m_shape); }
+
+    TensorSpan<T> sub(uint32_t index) {
+          cspan<uint32_t> sub_shape = m_shape.pop_back();
+          return TensorSpan<T>(m_data + index * Product<size_t>(sub_shape), sub_shape);
+    }
+    TensorSpan<const T> sub(uint32_t index) const {
+          cspan<uint32_t> sub_shape = cspan<uint32_t>(m_shape).pop_back();
+          return TensorSpan<const T>(m_data.data() + index * Product<size_t>(sub_shape), sub_shape);
+    }
+
+    size_t offset(cspan<uint32_t> index) const {
+        size_t out = index[0];
+        for (size_t i = 1; i < index.size(); i++) out = out * m_shape[i] + index[i];
+        return out;
+    }
+
+    void operator=(const TensorSpan<const float>& o) {
+        Copy(o.shape(), m_shape);
+        m_data.resize(o.size());
+        // TODO(Marko) not safe when strides are added to TensorSpan
+        std::copy(o.data(), o.data() + o.size(), m_data.data());
+    }
+
+    void reshape(cspan<uint32_t> shape, T init = T()) {
+        Copy(shape, m_shape);
+        m_data.resize(Product<size_t>(m_shape), init);
     }
 
    private:
