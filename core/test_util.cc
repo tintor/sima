68aa1baff6f66e33864641fccd267aa3e2380f79
Marko Tintor <marko@tintor.net>
2020-05-20 09:23:22 -0700

    Link test_util.cc

--- core/test_util.cc
+++ core/test_util.cc
@@ -1,8 +1,8 @@
-#include <core/range.h>
-#include <core/util.h>
-#include <core/string_util.h>
 #include <core/align_alloc.h>
 #include <core/format.h>
+#include <core/range.h>
+#include <core/string_util.h>
+#include <core/util.h>
 #include <immintrin.h>
 
 #include <catch.hpp>
@@ -17,7 +17,7 @@ TEST_CASE("min", "[util]") {
 // Two changes to reduce numerical error:
 // - double as accumulator even if T is float
 // - recursive subdivision implementation reduces numerical error to O(log(N)) from O(N) of naive algorithm
-template<typename T>
+template <typename T>
 double FSum(cspan<T> v) {
     if (v.size() >= 4) return FSum(v.first(v.size() / 2)) + FSum(v.pop_front(v.size() / 2));
     if (v.size() == 3) return v[0] + v[1] + v[2];
@@ -26,7 +26,7 @@ double FSum(cspan<T> v) {
     return 0;
 }
 
-template<typename T>
+template <typename T>
 double M2(cspan<T> v, double mean) {
     if (v.size() >= 4) return M2(v.first(v.size() / 2), mean) + M2(v.pop_first(v.size() / 2), mean);
     if (v.size() == 3) return sqr(v[0] - mean) + sqr(v[1] - mean) + sqr(v[2] - mean);
@@ -35,54 +35,44 @@ double M2(cspan<T> v, double mean) {
     return 0;
 }
 
-template<typename T>
+template <typename T>
 double Mean(cspan<T> v) {
     return FSum(v) / v.size();
 }
 
-template<typename T>
+template <typename T>
 double Variance(cspan<T> v, double mean) {
     return M2(v, mean) / v.size();
 }
 
-template<typename T>
+template <typename T>
 T NaiveSum(cspan<float> v) {
     T a = 0;
     for (auto e : v) a += e;
     return a;
 }
 
-template<typename T>
+template <typename T>
 T NaiveSum(cspan<double> v) {
     T a = 0;
     for (auto e : v) a += e;
     return a;
 }
 
-template<typename T>
+template <typename T>
 T ShardedSum2(cspan<float> v) {
     auto n = v.size() / 8;
-    return NaiveSum<T>(v.subspan(n*0, n)) +
-           NaiveSum<T>(v.subspan(n*1, n)) +
-           NaiveSum<T>(v.subspan(n*2, n)) +
-           NaiveSum<T>(v.subspan(n*3, n)) +
-           NaiveSum<T>(v.subspan(n*4, n)) +
-           NaiveSum<T>(v.subspan(n*5, n)) +
-           NaiveSum<T>(v.subspan(n*6, n)) +
-           NaiveSum<T>(v.subspan(n*7, n));
+    return NaiveSum<T>(v.subspan(n * 0, n)) + NaiveSum<T>(v.subspan(n * 1, n)) + NaiveSum<T>(v.subspan(n * 2, n)) +
+           NaiveSum<T>(v.subspan(n * 3, n)) + NaiveSum<T>(v.subspan(n * 4, n)) + NaiveSum<T>(v.subspan(n * 5, n)) +
+           NaiveSum<T>(v.subspan(n * 6, n)) + NaiveSum<T>(v.subspan(n * 7, n));
 }
 
-template<typename T>
+template <typename T>
 T ShardedSum2(cspan<double> v) {
     auto n = v.size() / 8;
-    return NaiveSum<T>(v.subspan(n*0, n)) +
-           NaiveSum<T>(v.subspan(n*1, n)) +
-           NaiveSum<T>(v.subspan(n*2, n)) +
-           NaiveSum<T>(v.subspan(n*3, n)) +
-           NaiveSum<T>(v.subspan(n*4, n)) +
-           NaiveSum<T>(v.subspan(n*5, n)) +
-           NaiveSum<T>(v.subspan(n*6, n)) +
-           NaiveSum<T>(v.subspan(n*7, n));
+    return NaiveSum<T>(v.subspan(n * 0, n)) + NaiveSum<T>(v.subspan(n * 1, n)) + NaiveSum<T>(v.subspan(n * 2, n)) +
+           NaiveSum<T>(v.subspan(n * 3, n)) + NaiveSum<T>(v.subspan(n * 4, n)) + NaiveSum<T>(v.subspan(n * 5, n)) +
+           NaiveSum<T>(v.subspan(n * 6, n)) + NaiveSum<T>(v.subspan(n * 7, n));
 }
 
 float ShardedSumVectorizedF(cspan<float> v) {
@@ -97,7 +87,7 @@ float ShardedSumSimdF(cspan<float> v) {
     return a[0] + a[1] + a[2] + a[3];
 }
 
-template<typename T>
+template <typename T>
 T ShardedSum(cspan<float> v) {
     T a0 = 0;
     T a1 = 0;
@@ -132,91 +122,92 @@ T IncrementalMean(cspan<float> v) {
 
 TEST_CASE("sum", "[util][.]") {
     for (size_t seed = 0; seed < 5; seed++) {
-    std::mt19937_64 random(1);
-    std::uniform_real_distribution<float> dis(0, 1e9);
-    std::uniform_real_distribution<double> dis2(0, 1e9);
-    vector<float, AlignAlloc<float>> numbers(100000000);
-    //vector<double, AlignAlloc<double>> numbers2(500000);
-    for (float& e: numbers) e = dis(random);
-    //for (double& e: numbers2) e = dis2(random);
+        std::mt19937_64 random(1);
+        std::uniform_real_distribution<float> dis(0, 1e9);
+        std::uniform_real_distribution<double> dis2(0, 1e9);
+        vector<float, AlignAlloc<float>> numbers(100000000);
+        // vector<double, AlignAlloc<double>> numbers2(500000);
+        for (float& e : numbers) e = dis(random);
+        // for (double& e: numbers2) e = dis2(random);
 
-    vector<function<double()>> func;
-    /*func << L2(FSum<float>(numbers) / numbers.size());
-    func << L2(NaiveSum<float>(numbers) / numbers.size());
-    func << L2(ShardedSum2<float>(numbers) / numbers.size());
-    func << L2(NaiveSum<float>(numbers) / numbers.size());
-    func << L2(ShardedSum2<float>(numbers) / numbers.size());
-    func << L2(ShardedSum<float>(numbers) / numbers.size());
-    func << L2(NaiveSum<float>(numbers) / numbers.size());
-    func << L2(NaiveSum<float>(numbers) / numbers.size());
-    func << L2(ShardedSumSimdF(numbers) / numbers.size());
-    func << L2(ShardedSumVectorizedF(numbers) / numbers.size());
-    func << L2(NaiveSum<double>(numbers) / numbers.size());
-    func << L2(NaiveSum<double>(numbers) / numbers.size());
-    func << L2(ShardedSum<double>(numbers) / numbers.size());
-    func << L2(ShardedSum2<double>(numbers) / numbers.size());
-    func << L2(ShardedSum2<double>(numbers) / numbers.size());
-    func << L2(NaiveSum<double>(numbers2) / numbers2.size());
-    func << L2(ShardedSum2<double>(numbers2) / numbers2.size());
-    func << L2(NaiveSum<double>(numbers2) / numbers2.size());
-    func << L2(ShardedSum2<double>(numbers2) / numbers2.size());
-    func << L2(NaiveSum<long double>(numbers) / numbers.size());
-    func << L2(IncrementalMean<float>(numbers));
-    func << L2(IncrementalMean<double>(numbers));*/
+        vector<function<double()>> func;
+        /*func << L2(FSum<float>(numbers) / numbers.size());
+        func << L2(NaiveSum<float>(numbers) / numbers.size());
+        func << L2(ShardedSum2<float>(numbers) / numbers.size());
+        func << L2(NaiveSum<float>(numbers) / numbers.size());
+        func << L2(ShardedSum2<float>(numbers) / numbers.size());
+        func << L2(ShardedSum<float>(numbers) / numbers.size());
+        func << L2(NaiveSum<float>(numbers) / numbers.size());
+        func << L2(NaiveSum<float>(numbers) / numbers.size());
+        func << L2(ShardedSumSimdF(numbers) / numbers.size());
+        func << L2(ShardedSumVectorizedF(numbers) / numbers.size());
+        func << L2(NaiveSum<double>(numbers) / numbers.size());
+        func << L2(NaiveSum<double>(numbers) / numbers.size());
+        func << L2(ShardedSum<double>(numbers) / numbers.size());
+        func << L2(ShardedSum2<double>(numbers) / numbers.size());
+        func << L2(ShardedSum2<double>(numbers) / numbers.size());
+        func << L2(NaiveSum<double>(numbers2) / numbers2.size());
+        func << L2(ShardedSum2<double>(numbers2) / numbers2.size());
+        func << L2(NaiveSum<double>(numbers2) / numbers2.size());
+        func << L2(ShardedSum2<double>(numbers2) / numbers2.size());
+        func << L2(NaiveSum<long double>(numbers) / numbers.size());
+        func << L2(IncrementalMean<float>(numbers));
+        func << L2(IncrementalMean<double>(numbers));*/
 
-    func << L2(IncrementalMean<double>(numbers));
-    func << L2(ShardedSum2<double>(numbers) / numbers.size());
+        func << L2(IncrementalMean<double>(numbers));
+        func << L2(ShardedSum2<double>(numbers) / numbers.size());
 
-    vector<string_view> name;
-    /*name << "FSum<float>"sv;
-    name << "NaiveSum<float>"sv;
-    name << "ShardedSum2<float>"sv;
-    name << "NaiveSum<float>"sv;
-    name << "ShardedSum2<float>"sv;
-    name << "ShardedSum<float>"sv;
-    name << "NaiveSum<float>"sv;
-    name << "NaiveSum<float>"sv;
-    name << "ShardedSumSimdF"sv;
-    name << "ShardedSumVectorizedF"sv;
-    name << "NaiveSum<double>"sv;
-    name << "NaiveSum<double>"sv;
-    name << "ShardedSum<double>"sv;
-    name << "ShardedSum2<double>"sv;
-    name << "ShardedSum2<double>"sv;
-    name << "xNaiveSum<double>"sv;
-    name << "xShardedSum2<double>"sv;
-    name << "xNaiveSum<double>"sv;
-    name << "xShardedSum2<double>"sv;
-    name << "NaiveSum<ldouble>"sv;
-    name << "IncMean<float>"sv;
-    name << "IncMean<double>"sv;*/
+        vector<string_view> name;
+        /*name << "FSum<float>"sv;
+        name << "NaiveSum<float>"sv;
+        name << "ShardedSum2<float>"sv;
+        name << "NaiveSum<float>"sv;
+        name << "ShardedSum2<float>"sv;
+        name << "ShardedSum<float>"sv;
+        name << "NaiveSum<float>"sv;
+        name << "NaiveSum<float>"sv;
+        name << "ShardedSumSimdF"sv;
+        name << "ShardedSumVectorizedF"sv;
+        name << "NaiveSum<double>"sv;
+        name << "NaiveSum<double>"sv;
+        name << "ShardedSum<double>"sv;
+        name << "ShardedSum2<double>"sv;
+        name << "ShardedSum2<double>"sv;
+        name << "xNaiveSum<double>"sv;
+        name << "xShardedSum2<double>"sv;
+        name << "xNaiveSum<double>"sv;
+        name << "xShardedSum2<double>"sv;
+        name << "NaiveSum<ldouble>"sv;
+        name << "IncMean<float>"sv;
+        name << "IncMean<double>"sv;*/
 
-    name << "IncMean<double>"sv;
-    name << "ShardedSum2<double>"sv;
+        name << "IncMean<double>"sv;
+        name << "ShardedSum2<double>"sv;
 
-    vector<ulong> ticks(name.size(), 0);
-    vector<Accumulator<double>> acc(name.size());
+        vector<ulong> ticks(name.size(), 0);
+        vector<Accumulator<double>> acc(name.size());
 
-    for (int cs = 0; cs < 10; cs++) {
-        std::shuffle(numbers.begin(), numbers.end(), random);
-        for (auto i : range(func.size())) {
-            const auto& f = func[i];
-            Timestamp begin;
-            double result = f();
-            Timestamp end;
-            ticks[i] += begin.elapsed(end);
-            acc[i] << result;
+        for (int cs = 0; cs < 10; cs++) {
+            std::shuffle(numbers.begin(), numbers.end(), random);
+            for (auto i : range(func.size())) {
+                const auto& f = func[i];
+                Timestamp begin;
+                double result = f();
+                Timestamp end;
+                ticks[i] += begin.elapsed(end);
+                acc[i] << result;
+            }
         }
-    }
 
-    ulong tmm = ticks[0];
-    for (auto e : ticks) tmm = min<ulong>(tmm, e);
-    double tm = tmm;
+        ulong tmm = ticks[0];
+        for (auto e : ticks) tmm = min<ulong>(tmm, e);
+        double tm = tmm;
 
-    vector<string> rows;
-    for (auto i : range(func.size()))
-        rows << format("%s|%.5f|%.5f|%f|%f", name[i], acc[i].mean(), acc[i].max() - acc[i].min(), sqrt(acc[i].variance()), ticks[i] / tm);
-    PrintTable(rows, '|', " ");
-    println();
+        vector<string> rows;
+        for (auto i : range(func.size()))
+            rows << format("%s|%.5f|%.5f|%f|%f", name[i], acc[i].mean(), acc[i].max() - acc[i].min(),
+                           sqrt(acc[i].variance()), ticks[i] / tm);
+        PrintTable(rows, '|', " ");
+        println();
     }
 }
