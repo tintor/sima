6f4389ac2b65be427b59010d37da56b5fa63ffb7
Marko Tintor <marko@tintor.net>
2020-05-25 01:38:08 -0700

    add core/optimizer

---
+++ core/optimizer.cc
@@ -0,0 +1,100 @@
+#include <core/optimizer.h>
+
+void Optimizer::Optimize(span<ParamT*> params) {
+    for (auto p : params) {
+        Timestamp ts;
+        EACH(p->v) p->v[i] -= alpha * p->g[i];
+        p->backward_ticks += ts.elapsed();
+    }
+}
+
+void Momentum::Optimize(span<ParamT*> params) {
+    if (ags.size() != params.size()) {
+        ags.resize(params.size());
+        for (auto i : range(params.size())) ags[i].reshape(params[i]->shape);
+    }
+
+    momentum_exp *= momentum;
+    float alpha_with_correction = alpha / (1 - momentum_exp);
+
+    for (auto j : range(params.size())) {
+        Timestamp ts;
+        vtensor &v = params[j]->v, &g = params[j]->g, &ag = ags[j];
+        EACH(v) {
+            ag[i] = momentum * ag[i] + (1 - momentum) * g[i];
+            v[i] -= alpha_with_correction * ag[i];
+        }
+        params[j]->backward_ticks += ts.elapsed();
+    }
+}
+
+template<bool Correct>
+void RMSPropUpdate(ParamT* p, tensor agg, float alpha, float rmsprop, float rmsprop_correction, float rmsprop_eps) {
+    Timestamp ts;
+    vtensor &v = p->v, &g = p->g;
+    EACH(v) {
+        agg[i] = rmsprop * agg[i] + (1 - rmsprop) * sqr(g[i]);
+        auto a = Correct ? (agg[i] * rmsprop_correction) : agg[i];
+        v[i] -= alpha * g[i] / sqrt(a + rmsprop_eps);
+    }
+    p->backward_ticks += ts.elapsed();
+}
+
+void RMSProp::Optimize(span<ParamT*> params) {
+    if (aggs.size() != params.size()) {
+        aggs.resize(params.size());
+        for (auto i : range(params.size())) aggs[i].reshape(params[i]->shape);
+    }
+
+    rmsprop_exp *= rmsprop;
+    float rmsprop_correction = 1 / (1 - rmsprop_exp);
+
+    if (rmsprop_correction >= 0.999999f) {
+        for (auto j : range(params.size())) {
+            RMSPropUpdate<false>(params[j], aggs[j], alpha, rmsprop, rmsprop_correction, rmsprop_eps);
+        }
+    } else {
+        for (auto j : range(params.size())) {
+            RMSPropUpdate<false>(params[j], aggs[j], alpha, rmsprop, 1, rmsprop_eps);
+        }
+    }
+}
+
+template<bool Correct>
+void AdamUpdate(ParamT* p, tensor ag, tensor agg, float alpha_with_correction, float momentum, float rmsprop, float rmsprop_correction, float rmsprop_eps) {
+    Timestamp ts;
+    vtensor &v = p->v, &g = p->g;
+    EACH(v) {
+        ag[i] = momentum * ag[i] + (1 - momentum) * g[i];
+        agg[i] = rmsprop * agg[i] + (1 - rmsprop) * sqr(g[i]);
+        auto a = Correct ? (agg[i] * rmsprop_correction) : agg[i];
+        v[i] -= alpha_with_correction * ag[i] / sqrt(a + rmsprop_eps);
+    }
+    p->backward_ticks += ts.elapsed();
+}
+
+void Adam::Optimize(span<ParamT*> params) {
+    if (ags.size() != params.size()) {
+        ags.resize(params.size());
+        aggs.resize(params.size());
+        for (auto i : range(params.size())) ags[i].reshape(params[i]->shape);
+        for (auto i : range(params.size())) aggs[i].reshape(params[i]->shape);
+    }
+
+    momentum_exp *= momentum;
+    float alpha_with_correction = alpha / (1 - momentum_exp);
+
+    rmsprop_exp *= rmsprop;
+    float rmsprop_correction = 1 / (1 - rmsprop_exp);
+
+    if (rmsprop_correction >= 0.999999f) {
+        for (auto j : range(params.size())) {
+            AdamUpdate<true>(params[j], ags[j], aggs[j], alpha_with_correction, momentum, rmsprop, rmsprop_correction, rmsprop_eps);
+        }
+    } else {
+        for (auto j : range(params.size())) {
+            AdamUpdate<false>(params[j], ags[j], aggs[j], alpha_with_correction, momentum, rmsprop, 1, rmsprop_eps);
+        }
+    }
+
+}
---
+++ core/optimizer.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <core/diff.h>
+
+struct Optimizer {
+    float alpha = 0.1;
+
+    virtual void Reset() {}
+    virtual void Optimize(span<ParamT*> params);
+};
+
+struct Momentum : public Optimizer {
+    float momentum = 0.9;
+
+    void Reset() override {
+        momentum_exp = 1;
+        for (auto& e : ags) EACH(e) e[i] = 0;
+    }
+    void Optimize(span<ParamT*> params) override;
+private:
+    float momentum_exp = 1;
+    vector<vtensor> ags;
+};
+
+struct RMSProp : public Optimizer {
+    float rmsprop = 0.999;
+    float rmsprop_eps = 1e-8;
+
+    void Reset() override {
+        rmsprop_exp = 1;
+        for (auto& e : aggs) EACH(e) e[i] = 0;
+    }
+    void Optimize(span<ParamT*> params) override;
+private:
+    float rmsprop_exp = 1;
+    vector<vtensor> aggs;
+};
+
+struct Adam : public Optimizer {
+    float momentum = 0.9;
+    float rmsprop = 0.999;
+    float rmsprop_eps = 1e-8;
+
+    void Reset() override {
+        rmsprop_exp = 1;
+        for (auto& e : ags) EACH(e) e[i] = 0;
+        for (auto& e : aggs) EACH(e) e[i] = 0;
+    }
+    void Optimize(span<ParamT*> params) override;
+private:
+    float momentum_exp = 1;
+    float rmsprop_exp = 1;
+    vector<vtensor> ags, aggs;
+};
