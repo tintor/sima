7343cb48f773da8b76d1ca92c48b448039815d4d
Marko Tintor <marko@tintor.net>
2020-06-19 20:30:49 -0700

    Add cout mutex to Check() and Fail()

---
+++ core/std.cc
@@ -0,0 +1,31 @@
+#include <core/std.h>
+#include <core/callstack.h>
+#include <mutex>
+
+static std::mutex g_cout_mutex;
+
+void Check(bool value, string_view message, const char* file, unsigned line, const char* function) {
+    if (value) return;
+
+    {
+        std::unique_lock lock(g_cout_mutex);
+        cout << "Check failed in " << function << " at " << file << ':' << line << " with message: " << message << endl;
+        string s;
+        Callstack().write(s, {});
+        cout << s;
+        cout.flush();
+    }
+    exit(0);
+}
+
+void Fail(string_view message, const char* file, unsigned line, const char* function) {
+    {
+        std::unique_lock lock(g_cout_mutex);
+        cout << "Failed in " << function << " at " << file << ':' << line << " with message: " << message << endl;
+        string s;
+        Callstack().write(s, {});
+        cout << s;
+        cout.flush();
+    }
+    exit(0);
+}
--- core/std.h
+++ core/std.h
@@ -101,18 +101,8 @@ overloaded(Ts...)->overloaded<Ts...>;  // not needed as of C++20
 constexpr double PI = M_PI;
 constexpr double INF = numeric_limits<double>::infinity();
 
-#include <core/callstack.h>
-
-inline void Check(bool value, string_view message = "", const char* file = __builtin_FILE(),
-                  unsigned line = __builtin_LINE(), const char* function = __builtin_FUNCTION()) {
-    if (value) return;
-    cout << "Check failed in " << function << " at " << file << ':' << line << " with message: " << message << endl;
-    string s;
-    Callstack().write(s, {});
-    cout << s;
-    cout.flush();
-    exit(0);
-}
+void Check(bool value, string_view message = "", const char* file = __builtin_FILE(),
+                  unsigned line = __builtin_LINE(), const char* function = __builtin_FUNCTION());
 
 #if 0
 #define DCheck(A, B) Check(A, B)
@@ -120,12 +110,5 @@ inline void Check(bool value, string_view message = "", const char* file = __bui
 #define DCheck(A, B)
 #endif
 
-inline void Fail(string_view message = "", const char* file = __builtin_FILE(), unsigned line = __builtin_LINE(),
-                 const char* function = __builtin_FUNCTION()) {
-    cout << "Failed in " << function << " at " << file << ':' << line << " with message: " << message << endl;
-    string s;
-    Callstack().write(s, {});
-    cout << s;
-    cout.flush();
-    exit(0);
-}
+void Fail(string_view message = "", const char* file = __builtin_FILE(), unsigned line = __builtin_LINE(),
+                 const char* function = __builtin_FUNCTION());
